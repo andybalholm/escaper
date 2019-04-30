6fc5838d78bcd912ed9cfa5394bdf4b3586083c9
Andy Balholm <andy@balholm.com>
2019-04-30 15:02:29 -0700

    Use brotli.HTTPCompressor in ForHTTP.

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/andybalholm/escaper
 go 1.12
 
 require (
-	github.com/andybalholm/brotli v0.0.0-20190425160332-f00818cf3661
-	github.com/golang/gddo v0.0.0-20190419222130-af0f2af80721
+	github.com/andybalholm/brotli v0.0.0-20190430215306-5c318f9037cb
+	github.com/google/go-cmp v0.3.0 // indirect
 )
--- http.go
+++ http.go
@@ -1,43 +1,18 @@
 package escaper
 
 import (
-	"compress/gzip"
 	"io"
 	"net/http"
 
 	"github.com/andybalholm/brotli"
-	"github.com/golang/gddo/httputil"
 )
-
-type nopCloser struct{}
-
-func (nopCloser) Close() error { return nil }
 
 // ForHTTP returns an Escaper for an HTTP request. It compresses the response
 // as specified in the Accept-Encoding header, and sets the Content-Type and
 // Content-Encoding headers appropriately. The returned Closer must be closed
 // before the HTTP handler returns.
 func ForHTTP(w http.ResponseWriter, r *http.Request) (*Escaper, io.Closer) {
-	var dest io.Writer = w
-	var closer io.Closer = nopCloser{}
-
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-
-	if w.Header().Get("Content-Encoding") == "" {
-		encoding := httputil.NegotiateContentEncoding(r, []string{"br", "gzip"})
-		switch encoding {
-		case "br":
-			bw := brotli.NewWriter(dest)
-			dest = bw
-			closer = bw
-			w.Header().Set("Content-Encoding", "br")
-		case "gzip":
-			gw := gzip.NewWriter(dest)
-			dest = gw
-			closer = gw
-			w.Header().Set("Content-Encoding", "gzip")
-		}
-	}
-
-	return New(dest), closer
+	c := brotli.HTTPCompressor(w, r)
+	return New(c), c
 }
